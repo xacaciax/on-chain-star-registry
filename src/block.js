e08a2bfa1a0f03df5e1a58c85497318b7874465f
Acacia Pappas <acacia.m.pappas@gmail.com>
2021-12-04 16:55:45 -0800

    initial commit, implement validate and getBData methods in Block.js

---
+++ src/block.js
@@ -0,0 +1,99 @@
+/**
+ *                          Block class
+ *  The Block class is a main component into any Blockchain platform,
+ *  it will store the data and act as a dataset for your application.
+ *  The class will expose a method to validate the data... The body of
+ *  the block will contain an Object that contain the data to be stored,
+ *  the data should be stored encoded.
+ *  All the exposed methods should return a Promise to allow all the methods
+ *  run asynchronous.
+ */
+
+// Block data model
+// {
+//    "hash": "000000000000e198e04b0759cfecd59a95604cc551db9ad3b12a290375a95419",
+//    "height": 1325438,
+//    "body": [],
+//    "time": 1529001822,
+//    "previousBlockHash": "00000000af61e2a9b09936dad3f5f213a8f659c5845d142b71894b4d5e5e17da"
+//  }
+
+const SHA256 = require("crypto-js/sha256");
+const hex2ascii = require("hex2ascii");
+
+class Block {
+  // Constructor - argument data will be the object containing the transaction data
+  constructor(data) {
+    this.hash = null; // Hash of the block
+    this.height = 0; // Block Height (consecutive number of each block)
+    this.body = Buffer.from(JSON.stringify(data)).toString("hex"); // Will contain the transactions stored in the block, by default it will encode the data
+    this.time = 0; // Timestamp for the Block creation
+    this.previousBlockHash = null; // Reference to the previous Block Hash
+  }
+
+  /**
+   *  validate() method will validate if the block has been tampered or not.
+   *  Been tampered means that someone from outside the application tried to change
+   *  values in the block data as a consecuence the hash of the block should be different.
+   *
+   * Steps:
+   * 1. Return a new promise to allow the method be called asynchronous.
+   * 2. Create an auxiliary variable and store the current hash of the block in it (this represent the block object)
+   * 3. Recalculate the hash of the entire block (Use SHA256 from crypto-js library)
+   * 4. Compare if the auxiliary hash value is different from the calculated one.
+   * 5. Resolve true or false depending if it is valid or not.
+   */
+  validate() {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      // Save in auxiliary variable the current block hash
+      let currentHash = self.hash;
+
+      // Recalculate the hash of the Block
+      let freshHash = SHA256(JSON.stringify(self)).toString();
+
+      // Comparing if the hashes changed
+      let isValid = freshHash === currentHash;
+
+      if (!isValid) {
+        // Returning the Block is not valid
+        return resolve(false);
+      } else {
+        // Returning the Block is valid
+        return resolve(true);
+      }
+    });
+  }
+
+  /**
+   *  Auxiliary Method to return the block body (decoding the data)
+   *  Steps:
+   *  1. Use hex2ascii module to decode the data
+   *  2. Because data is a javascript object use JSON.parse(string) to get the Javascript Object
+   *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block`
+   *     or Reject with an error.
+   */
+  getBData() {
+    let self = this;
+
+    return new Promise((resolve, reject) => {
+      // Getting the encoded data saved in the Block
+      let data = self.body;
+
+      // Decoding the data to retrieve the JSON representation of the object
+      let json = hex2ascii(data);
+
+      // Parse the data to an object to be retrieved.
+      let blockObject = JSON.parse(json);
+
+      // Resolve with the data if the object isn't the Genesis block
+      if (self.height > 0) {
+        return resolve(blockObject);
+      } else {
+        return reject(Error("No data in the genesis block."));
+      }
+    });
+  }
+}
+
+module.exports.Block = Block; // Exposing the Block class as a module
