4f93d49fd95212728673b2a8ecb7eccdf5f7c5d9
Acacia Pappas <acacia.m.pappas@gmail.com>
2021-12-06 01:26:36 -0800

    example request body and response for /submitstar

---
+++ src/dataFixtures/starBlockRequestBody.js
@@ -0,0 +1,21 @@
+
+// Example of request body for /submitstar
+{
+    "address": "12xVsEGAbhX5HtNGnYnRb2q88EmJdBjJSA",
+    "signature": "HyuHxS+mFRqEkw5SaM/8hZKq3y4HClaLyUymHaTj/zFrcJCqra5big3vZUMXF1GiRrSvIhEE40c+aHJGaX42AcE=",
+    "message": "12xVsEGAbhX5HtNGnYnRb2q88EmJdBjJSA:1638748916:starRegistry",
+    "star": {
+        "dec": "68° 52' 56.9",
+        "ra": "16h 29m 1.0s",
+        "story": "Testing the story 4"
+	}
+}
+
+// Example of block response
+{
+    "hash": "d4b985c28967c2424ccc4f84326dd0fe8beebfcb8e97f8f1b5c1e8bfd9745a0b",
+    "height": 4,
+    "body": "7b22646563223a223638c2b0203532272035362e39222c227261223a223136682032396d20312e3073222c2273746f7279223a2254657374696e67207468652073746f72792034227d",
+    "time": "1638777358",
+    "previousBlockHash": "20c5f5d76106ecb3330807bfa805deb6506cc47926a603d46422504f3fef5535"
+}
